38d9ef68e00b2c876f2f47f863d8dde99ab4c2cd
Mukul <mukul1904@gmail.com>
2018-01-13 23:31:04 +0530

    Add messages

---
+++ messages.js
@@ -0,0 +1,12 @@
+
+
+module.exports = {
+  cardGreeting: () => 'Welcome to Movie Quotes',
+  messageGreeting: () => 'Welcome to Movie Quotes. Get a random movie random quotes by saying \'give me a random quote\'',
+  repromptGreeting: () => 'I\'m sorry, I am not able to hear your request. Please repeat or say \'help\' for sample requests',
+  cardHelp: () => 'Help from Movie Quotes',
+  messageHelp: () => 'You can ask this skills, \'give me a random movie quotes\'',
+  cardGoodBye: () => 'Good Bye from Movie Quotes',
+  messageGoodBye: () => 'Thank you for using Movie Quotes',
+  messageReprompt: () => 'I\'m sorry, I am not able to hear your request. Please repeat or say \'help\' for sample requests',
+};
